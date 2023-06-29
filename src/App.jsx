6dc4aa8ecab5972162d7ddeb8ed01f30771b1494
Felipe Soller <soller@outlook.com>
2023-06-28 21:13:18 -0300

    Css Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // JXS = JavaScript + XML (HTML)
 
 import { Post } from "./Post"
+import { Header } from "./components/Header"
 
 export function App() {
 
   return (
     <div>
+      <Header />
       <Post
         author="Felipe Soller"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem eligendi nemo dicta cumque ea quisquam delectus. Harum sunt porro iure odit dolores quos voluptatibus nobis earum eum deserunt, praesentium veritatis."
