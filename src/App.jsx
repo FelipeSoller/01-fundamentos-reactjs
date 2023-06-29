3ad95fbf0bb95580214c8e6f41523cb9e32e3584
Felipe Soller <soller@outlook.com>
2023-06-29 12:39:24 -0300

    Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,29 @@
 
 import { Post } from "./Post"
 import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar"
 
 import './global.css'
+import styles from './App.module.css'
 
 export function App() {
 
   return (
     <div>
       <Header />
-      <Post
-        author="Felipe Soller"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem eligendi nemo dicta cumque ea quisquam delectus. Harum sunt porro iure odit dolores quos voluptatibus nobis earum eum deserunt, praesentium veritatis."
-      />
-      <Post
-        author="Diego Fernandes"
-        content="Um novo post muito legal"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+        <Post
+          author="Felipe Soller"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem eligendi nemo dicta cumque ea quisquam delectus. Harum sunt porro iure odit dolores quos voluptatibus nobis earum eum deserunt, praesentium veritatis."
+        />
+        <Post
+          author="Diego Fernandes"
+          content="Um novo post muito legal"
+        />
+        </main>
+      </div>
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,20 @@
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1620503374956-c942862f0372?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=30"
+      />
+      <div className={styles.profile}>
+        <strong>Felipe Soller</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">Editar perfil</a>
+      </footer>
+    </aside>
+  )
+}
