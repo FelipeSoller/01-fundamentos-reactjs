50a396f71950aa3394b062e5fb0cf94cda79c6fc
Felipe Soller <felipe.souza@imaginedone.com.br>
2023-06-30 16:43:48 -0300

    Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,38 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+
+import styles from './Comment.module.css'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/62728038?v=4" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Felipe Soller</strong>
+              <time dateTime="2023-06-29 14:15:00" title="29 de junho de 2023">
+                Publicado há 1h
+              </time>
+            </div>
+
+            <button title='Deletar comentário'>
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>
+            Muito bom, Devon. Parabéns!!
+          </p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
