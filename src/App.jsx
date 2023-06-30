aac3b4d10860b93c0b78a0f7080ea5587c097941
Felipe Soller <felipe.souza@imaginedone.com.br>
2023-06-30 15:10:38 -0300

    Post

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 // JXS = JavaScript + XML (HTML)
 
-import { Post } from "./Post"
 import { Header } from "./components/Header"
 import { Sidebar } from "./components/Sidebar"
 
 import './global.css'
 import styles from './App.module.css'
+import { Post } from "./components/Post"
 
 export function App() {
 
@@ -15,14 +15,8 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-        <Post
-          author="Felipe Soller"
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem eligendi nemo dicta cumque ea quisquam delectus. Harum sunt porro iure odit dolores quos voluptatibus nobis earum eum deserunt, praesentium veritatis."
-        />
-        <Post
-          author="Diego Fernandes"
-          content="Um novo post muito legal"
-        />
+        <Post />
+        <Post />
         </main>
       </div>
     </div>
---
+++ src/components/Post.jsx
@@ -0,0 +1,36 @@
+import styles from './Post.module.css'
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/62728038?v=4" />
+          <div className={styles.authorInfo}>
+            <strong>Felipe Soller</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time dateTime="2023-06-29 14:15:00" title="29 de junho de 2023">
+          Publicado há 1h
+        </time>
+      </header>
+      <div className={styles.content}>
+        <p>
+          Fala galeraa 👋
+        </p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          <a href="#">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="#">#novoprojeto</a>{' '}
+          <a href="#">#nlw</a>{' '}
+          <a href="#">#rocketseat</a>
+        </p>
+      </div>
+    </article>
+  )
+}
