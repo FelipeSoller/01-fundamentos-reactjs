81a62a098817fcb16f00399d05c4ac4b86237802
Felipe Soller <soller@outlook.com>
2023-07-04 15:51:10 -0300

    using props to render posts

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,38 +1,39 @@
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 
 import styles from './Post.module.css'
 
-export function Post() {
+export function Post({ author, content, publishedAt }) {
+  const publishedAtDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH':'mm", { locale: ptBR })
+  const publishedAtDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  })
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://avatars.githubusercontent.com/u/62728038?v=4" />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Felipe Soller</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
-        <time dateTime="2023-06-29 14:15:00" title="29 de junho de 2023">
-          Publicado há 1h
+        <time dateTime={publishedAtDateFormatted} title={publishedAtDateFormatted}>
+          {publishedAtDateRelativeToNow}
         </time>
       </header>
       <div className={styles.content}>
-        <p>
-          Fala galeraa 👋
-        </p>
-        <p>
-          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-        </p>
-        <p>
-          <a href="#">jane.design/doctorcare</a>
-        </p>
-        <p>
-          <a href="#">#novoprojeto</a>{' '}
-          <a href="#">#nlw</a>{' '}
-          <a href="#">#rocketseat</a>
-        </p>
+        {content.map(item => {
+          if (item.type === 'paragraph') {
+            return <p>{item.content}</p>
+          } else if (item.type === 'link')
+            return <p><a href='#'>{item.content}</a></p>
+        })}
       </div>
 
       <form className={styles.commentForm}>
